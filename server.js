2901af914a5a9290a5333a9ffc086d920722959e
MohammedAlMaqbli <Mohammed.alMaqbli@Gmail.com>
2022-09-04 21:40:13 -0400

    Update server.js

--- server.js
+++ server.js
@@ -12,12 +12,43 @@ const server = express()
 
 const io = socketIO(server);
 
+const users = [];
+const members = [];
+
 io.on("connection", (socket) => {
-  console.log("Client connected " + PORT);
-  socket.on("disconnect", () => console.log("Client disconnected"));
+  console.log("a user connected");
+
+  socket.on("user_connected", function (user_id) {
+    users[user_id] = socket.id;
+    io.emit("updateUserStatus", users);
+    console.log("user connected " + user_id);
+  });
+
+  socket.on("member_connected", function (user_id) {
+    members[user_id] = socket.id;
+    io.emit("updateMemberStatus", members);
+    console.log("member connected " + user_id);
+  });
+
+  socket.on("disconnect", function () {
+    var i = users.indexOf(socket.id);
+    users.splice(i, 1, 0);
+    io.emit("updateUserStatus", users);
+  });
+
+  socket.on("send_message", function (message) {
+    io.to(`${users[message.receiver_id]}`).emit("receive_message", message);
+  });
+
+  socket.on("send_message-member", function (message) {
+    io.to(`${members[message.receiver_id]}`).emit(
+      "receive_message-member",
+      message
+    );
+  });
 });
 
 setInterval(
-  () => io.emit("time", new Date().toTimeString() + " " + PORT),
+  () => io.emit("time", new Date().toTimeString() + " Moe " + PORT),
   1000
 );
