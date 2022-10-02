e8efc6430d31e2a89ee5749cd86c8a9575eed4f0
MohammedAlMaqbli <Mohammed.alMaqbli@Gmail.com>
2022-10-02 06:43:31 -0400

    Update server.js

--- server.js
+++ server.js
@@ -62,7 +62,9 @@ io.on("connection", (socket) => {
   //on change seen status
   socket.on("change_seen_status", function (message) {
     console.log("Change seen status: " + users[message.receiver_id], message);
-    io.to(`${users[message.receiver_id]}`).emit("seen_status", message);
+    //if sender is online
+    if (users[message.sender_id])
+      io.to(`${users[message.receiver_id]}`).emit("seen_status", message);
   });
 });
 
